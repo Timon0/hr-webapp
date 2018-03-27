db1c8a1e386b130938daef07122f226cc879c50a
DESKTOP-SE466MC\bbzw <timon.kurmann@gmail.com>
2018-03-27 09:22:12 +0200

    Add CreateDB script

---
+++ CreatHrSystemDatabase.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE HRSystem
+go
+use HRSystem
+go
+
+
+CREATE TABLE Department (
+	DepartmentId INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Name] VARCHAR(100) NOT NULL,
+	[Description] VARCHAR(200) NOT NULL,
+)
+
+CREATE TABLE Project (
+	ProjectId INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Name] VARCHAR(100) NOT NULL,
+	[Description] VARCHAR(200) NOT NULL,
+)
+
+CREATE TABLE Place (
+	PlaceId INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	PostalCode INT NOT NULL,
+	Place VARCHAR(200) NOT NULL,
+)
+
+CREATE TABLE Employee (
+	EmployeeId INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	Firstname VARCHAR(100) NOT NULL,
+	Lastname VARCHAR(100) NOT NULL,
+	Birthday date NOT NULL,
+	BossId INT NULL,
+	Salary INT NULL,
+	[Address] VARCHAR(200) NULL,
+	FkDepartment INT FOREIGN KEY REFERENCES Department(DepartmentId) NOT NULL,
+	FkPlace INT FOREIGN KEY REFERENCES PLACE(PlaceId) NOT NULL,
+)
+
+CREATE TABLE EmployeeProject (
+	ProjectId INT FOREIGN KEY REFERENCES Project(ProjectId) NOT NULL,
+	EmployeeId INT FOREIGN KEY REFERENCES Employee(EmployeeId)  NOT NULL,
+	CONSTRAINT primaryKey PRIMARY KEY (ProjectId, EmployeeId),
+)
