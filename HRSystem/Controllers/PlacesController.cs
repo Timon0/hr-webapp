d39781cb53b297df863cc159c2d6904bd6d390a6
DESKTOP-SE466MC\bbzw <timon.kurmann@gmail.com>
2018-04-17 11:08:56 +0200

    places view

---
+++ HRSystem/Controllers/PlacesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRSystem.Models;
+using HRSystem.Data;
+
+namespace HRSystem.Controllers
+{
+    public class PlacesController : Controller
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork(new HRSystemEntities());
+
+        // GET: Places
+        public ActionResult Index()
+        {
+            return View(unitOfWork.Places.GetAll());
+        }
+
+        // GET: Places/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = unitOfWork.Places.SingleOrDefault(p => p.PlaceId == id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // GET: Places/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Places/Create
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlaceId,PostalCode,Place1")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.Places.Add(place);
+                unitOfWork.Complete();
+                return RedirectToAction("Index");
+            }
+
+            return View(place);
+        }
+
+        // GET: Places/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = unitOfWork.Places.SingleOrDefault(p => p.PlaceId == id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // POST: Places/Edit/5
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlaceId,PostalCode,Place1")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.Places.Update(place);
+                unitOfWork.Complete();
+                return RedirectToAction("Index");
+            }
+            return View(place);
+        }
+
+        // GET: Places/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = unitOfWork.Places.SingleOrDefault(p => p.PlaceId == id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // POST: Places/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Place place = unitOfWork.Places.SingleOrDefault(p => p.PlaceId == id);
+            unitOfWork.Places.Remove(place);
+            unitOfWork.Complete();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                unitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
